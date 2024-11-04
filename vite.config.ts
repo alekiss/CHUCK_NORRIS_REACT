2716daefd691432902dc8ff294f2d7783334e618
alekiss <alekiss.melo@gmail.com>
2024-11-04 18:26:15 -0300

    feat: configurações de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/CHUCK_NORRIS_REACT" //Adicionar um /<nomedorepositório>, importante salientar que não é o nome do projeto e sim do repo do github
 })
